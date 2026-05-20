c6f9cfec0fb6a9d541cb7a6285f598f9d2bddebc
ditverse <radityarizki2309@gmail.com>
2026-05-20 23:16:12 +0700

    fix connection database

--- config/cors.go
+++ config/cors.go
@@ -7,7 +7,7 @@ import (
 // CorsConfig returns the CORS configuration for the app
 func CorsConfig() cors.Config {
 	return cors.Config{
-		AllowOrigins:     "*",
+		AllowOrigins:     "http://localhost:8080, http://127.0.0.1:8080",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowMethods:     "GET, POST, HEAD, PUT, DELETE, PATCH, OPTIONS",
 		AllowCredentials: true,
